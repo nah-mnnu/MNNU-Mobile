7551cfe38029108bc6a5ee9feb3d22fe256e2e08
Sam Course <notcourseps@gmail.com>
2022-11-16 15:03:45 +0100

    Add MNNU test ledger

--- core/configs/ledgers/indy/index.ts
+++ core/configs/ledgers/indy/index.ts
@@ -1,6 +1,27 @@
-import { IndyPoolConfig } from '@aries-framework/core'
+import {IndyPoolConfig} from '@aries-framework/core'
 
-// @ts-ignore
-import ledgers from './ledgers.json'
+import BCovrinTest from './bcovrin-test/pool-config'
+import CandyDev from './candy-dev/pool-config'
+import CandyProd from './candy-prod/pool-config'
+import CandyTest from './candy-test/pool-config'
+import IndicioDemoNet from './indicio-demo-net/pool-config'
+import IndicioMainNet from './indicio-main-net/pool-config'
+import IndicioTestNet from './indicio-test-net/pool-config'
+import SovrinBuilderNet from './sovrin-builder-net/pool-config'
+import SovrinMainNet from './sovrin-main-net/pool-config'
+import SovrinStagingNet from './sovrin-staging-net/pool-config'
+import MnnuTest from './mnnu-test/pool-config'
 
-export default ledgers as IndyPoolConfig[]
+export default [
+    SovrinMainNet,
+    IndicioMainNet,
+    SovrinStagingNet,
+    IndicioDemoNet,
+    IndicioTestNet,
+    CandyDev,
+    CandyTest,
+    CandyProd,
+    BCovrinTest,
+    SovrinBuilderNet,
+    MnnuTest,
+] as IndyPoolConfig[]
